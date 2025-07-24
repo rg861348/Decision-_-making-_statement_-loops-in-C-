e53b05475d76f7460a28f09947e81359259f7bb3
rg861348 <rg861348@gmail.com>
2025-07-24 13:04:31 +0530

    Create Exp_6_2 for_ loop_ printing _str_5 times.cpp

---
+++ Exp_6_2 for_ loop_ printing _str_5 times.cpp
@@ -0,0 +1,22 @@
+// RISHITA GOSWAMI 
+// 24070123086
+// B1
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  for(int i = 0;i < 5;i++){
+    cout << "SIT "<<endl;
+  }
+  return 0;
+}
+
+/*
+output:
+SIT 
+SIT
+SIT
+SIT
+SIT
+*/
